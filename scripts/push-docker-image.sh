1e115b476503ef5117aa1641a40c3ee8f1581fad
Ikenna Omekam <iomekam@github.com>
2020-06-23 10:56:59 -0700

    implementation and deploy script

---
+++ scripts/push-docker-image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Parse arguments.
+while getopts 'c:u:p:' arg; do
+  case "${arg}" in
+    c) CONTAINER_REGISTRY=${OPTARG} ;;
+    u) CONTAINER_REGISTRY_USERNAME=${OPTARG} ;;
+    p) CONTAINER_REGISTRY_PASSWORD=${OPTARG} ;;
+  esac
+done
+
+DOCKER_SOCKET=/var/run/docker.sock
+
+docker build -t push-docker-image . --file Dockerfile.push-docker-image
+docker run \
+  --mount src=$DOCKER_SOCKET,target=$DOCKER_SOCKET,type=bind \
+  --env DOCKER_IMAGE_NAME=push-docker-image \
+  --env CONTAINER_REGISTRY=$CONTAINER_REGISTRY \
+  --env CONTAINER_REGISTRY_USERNAME=$CONTAINER_REGISTRY_USERNAME \
+  --env CONTAINER_REGISTRY_PASSWORD=$CONTAINER_REGISTRY_PASSWORD \
+  push-docker-image
--- scripts/push-docker-image/push-docker-image.sh
+++
@@ -1,2 +0,0 @@
-docker build -t push-docker-image . --file Dockerfile.push-docker-image
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock push-docker-image
